c245d8784b15852bac6d4364a8287e143c12bd27
Dolly.Singh <Dolly.Singh@ticketmaster.com>
2020-10-15 11:31:19 +0530

    robot code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # robot-in-mines
+
+Step 1: I took advantage of the symmetry of the problem by calculating the area in the first quadrant and multiplying it with 4 to get the total area(all quadrant).
+
+Step 2: First step I found out all the boundary points of the required region. I  started from the origin(0,0). I fixed x to 0 and incremented y by 1 repeatedly. Then I calculated the sum of digits of both x and y every time and checked whether the sum is equal to 23. I stopped incrementing y when the sum of digits became 23.
+
+Step 3:After that, I set x to 1 and y to 0 and increment y repeatedly until the sum becomes 23. I did the same thing again and again until the final coordinate (599,0) is reached. After this coordinate, the robot cannot move ahead.
+
+Step 4:After finding out all the boundary points in the first quadrant I found out the area in the following manner.
+
+The robot moves on a grid so the area in first quadrant:
+x:0 , y:599
+x:1 , y:499
+x:2 , y:399
+x:3, y:299
+x:4, y:199
+....
+x:590, y:0
+area = 599*1+499*1+399*1+299*1+199*1 + ..... + 0*1 = 31020 which I  observe, is nothing but the sum of y coordinates of all the points. I used the above method for finding out the required area in the first quadrant.
+
+Step 5: But the robot can move in all the quadrants. So I multiplied the area obtained in the first quadrant by 4 to get the total area in all the four quadrants in which the robot can move that is 124080.
--- start.js
+++ start.js
@@ -1,35 +1,44 @@
-document.write("<h1>" + "CalmIsland Robot Solution" + "</h1>" + "</br>");
+document.write("<h2>" + "CalmIsland Robot Solution" + "</h2>");
 
+document.write("<h3>" + "Calculate area for first quadrant" + "</h3>");
+
+//First Quadrant
+//Start from origin(X=0, Y=0)
 var x = 0;
 var y = 0;
+
+//area is initialized with 0
 var area = 0;
+
 do {
   let X = x.toString();
   let Y = y.toString();
 
-  var arrX = X.split("").map(function (item) {
-    let sum = 0;
-    return (sum = sum + parseInt(item));
-  });
+  //Convert all digits in a numer to array e.g. X = 599 to [5,9,9]
+  var arrX = X.split("").map(item => parseInt(item));
 
-  var arrY = Y.split("").map(function (item) {
-    let sum = 0;
-    return (sum = sum + parseInt(item));
-  });
-  var totalY = 0,
-    totalX = 0;
-  arrY.forEach(function (item) {
-    totalY = totalY + item;
-  });
+  //Similarly for Y
+  var arrY = Y.split("").map(item => parseInt(item));
+
+  var totalY = 0, totalX = 0;
+
+  //Add every element on arrX
   arrX.forEach(function (item) {
     totalX = totalX + item;
   });
 
-  if (totalX + totalY == 23) {
-    area = area + y;
-    document.write("x:" + x + " , " + "y:" + y + "</br>");
+  //Add every element on arrY
+  arrY.forEach(function (item) {
+    totalY = totalY + item;
+  });
 
-    if (y == 0 && totalX + totalY == 23) {
+  //Check for dangerous points
+  if (totalX + totalY === 23) {
+    area = area + y;
+    document.write(`(x: ${x} , y: ${y})</br>`);
+
+    //Stop on x-axis and break the loop for first quadrant
+    if (y === 0 && totalX + totalY === 23) {
       break;
     }
 
@@ -37,10 +46,14 @@ do {
     y = 0;
     continue;
   }
+
   y++;
 } while (totalX + totalY <= 23);
 
 document.write("</br>" + "Area in First Quadrant:" + area + "</br>");
+
 var totalArea = 4 * area;
+
 document.write("Total area in all 4 Quadrants:" + totalArea + "</br>");
+
 document.write("The area in which robot can move:" + totalArea + "</br>");
